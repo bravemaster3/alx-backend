baa3a3e92a458d1a5db6214bb1bee61ce68ffcf1
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-14 17:12:17 +0000

    Add 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+basic Flask app
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """babel config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+def get_locale():
+    """gets locale from request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """renders a simple page"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
