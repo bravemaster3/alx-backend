752c39cd9c2d733805b838dae284d311a9b927b3
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-14 17:15:01 +0000

    Add 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,12 +20,10 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
 def get_locale():
     """gets locale from request"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
