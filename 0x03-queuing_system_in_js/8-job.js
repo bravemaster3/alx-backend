17bc18312fe8bd1267c2558a92976094edc7ffd4
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-06-11 17:48:51 +0000

    Add up to task 11

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,35 @@
+import kue from 'kue';
+
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_3', jobData);
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed: ${err}`);
+    });
+
+    job.on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+
+    // Save the job to the queue
+    job.save((err) => {
+      if (err) {
+        console.log(`Failed to create job: ${err}`);
+      } else {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
+  });
+}
+
+// Export the function
+export default createPushNotificationsJobs;
